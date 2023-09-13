2c4d454eb7eb9ff7a539ba7b88c0dcd98fc93219
icsboyx <obladio28@gmail.com>
2023-09-13 14:28:22 +0200

    Broadcast buss ok

--- src/chat_engine.rs
+++ src/chat_engine.rs
@@ -1,16 +1,12 @@
+use tokio::task::{ JoinError, JoinHandle };
+use tokio::sync::broadcast::{ self, Sender, Receiver };
+
 use crate::*;
 
-pub fn start_chat_engine(mut client_tx_bus: CommunicationBus<String>, client_rx_bus: &Bus<String>) {
-    new_thread!("chat_engine", {
-        loop {
-            match client_tx_bus.receiver.recv() {
-                Ok(message) => {
-                    println!("[CHAT_ENGINE]:{:#?}", message);
-                    client_rx_bus.broadcast(message);
-                }
-
-                Err(_) => todo!(),
-            }
+pub async fn start_chat_engine(mut rx: Receiver<String>) {
+    loop {
+        while let r = rx.recv().await.unwrap() {
+            println!("Bus received: {:?}", r);
         }
-    });
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,49 @@
 use mini_redis::Frame;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{ AsyncReadExt, AsyncWriteExt };
 use tokio::net::TcpStream;
-use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use tokio::sync::broadcast::{ self, Sender, Receiver };
+use tokio::sync::mpsc::{ self, UnboundedReceiver, UnboundedSender };
 use tokio::time::Duration;
+mod chat_engine;
+use chat_engine::*;
 
 #[tokio::main]
 async fn main() {
     let mut handles = Vec::new();
-    let (tx, mut rx): (UnboundedSender<String>, UnboundedReceiver<String>) =
-        mpsc::unbounded_channel();
+    let bus: (Sender<String>, Receiver<String>) = broadcast::channel(16);
+    let client_rx = bus.0.subscribe();
 
-    handles.push(tokio::spawn(async move {
-        let listener = tokio::net::TcpListener::bind("127.0.0.1:23456")
-            .await
-            .unwrap();
-        while let Ok((socket, _)) = listener.accept().await {
-            let tx_clone = tx.clone();
-            tokio::spawn(async move {
-                process(socket, tx_clone).await;
-            });
-        }
-    }));
-
-    handles.push(tokio::spawn(async move {
-        while let Some(r) = rx.recv().await {
-            println!("Bus received: {:?}", r);
-        }
-    }));
+    handles.push(tokio::spawn(start_chat_engine(bus.1)));
+    handles.push(
+        tokio::spawn(async move {
+            let listener = tokio::net::TcpListener::bind("127.0.0.1:23456").await.unwrap();
+            while let Ok((socket, _)) = listener.accept().await {
+                tokio::spawn(process(socket, bus.0.clone()));
+            }
+        })
+    );
 
     for handle in handles {
-        println!("GOT {:?}", handle.await.unwrap());
+        println!("GOT {:#?}", handle.await);
     }
 }
 
-async fn process(mut socket: TcpStream, tx: UnboundedSender<String>) {
+async fn process(mut socket: TcpStream, tx: Sender<String>) {
+    let mut rx = tx.subscribe();
     let mut buf = vec![0; 1024];
+    let id = socket.peer_addr().unwrap();
+    // tokio::spawn();
+    tokio::spawn(async move {
+        loop {
+            let value = rx.recv().await.unwrap();
+            println!("Client {:#?}, received {}", id.to_string(), value);
+        }
+    });
     loop {
-        let n = socket
-            .read(&mut buf)
-            .await
-            .expect("failed to read data from socket");
-        tx.send(String::from_utf8_lossy(&buf[..n]).to_string())
-            .expect("send failed");
-
+        let n = socket.read(&mut buf).await.expect("failed to read data from socket");
+        tx.send(String::from_utf8_lossy(&buf[..n]).to_string()).expect("send failed");
         if n == 0 {
             return;
         }
-
-        socket
-            .write_all(&buf[..n])
-            .await
-            .expect("failed to write data to socket");
     }
 }
